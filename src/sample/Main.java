95754ec4bea2d7915f12765dee39321cb06bdbc8
Tomasz Augustyn <lourian@poczta.fm>
2017-03-28 20:39:06 +0200

    Huge refactoring - application divided on Tab Controllers and Main Controller

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -30,28 +30,18 @@ public class Main extends Application {
     private Scene scene;
     private WordEntryList wordEntryList;
     private WordEntryList initialWordEntryList;
-    private ObservableList<WordEntry> data;
     private File loadedFile;
 
-    @FXML private TabLearningController tabLearningController;
-    @FXML private TextField filePath;
-    @FXML private AutoCompleteTextField wordField;
-    @FXML private AutoCompleteTextField translationField;
-    @FXML private Button fileChooser;
-    @FXML private Button deleteWordBtn;
-    @FXML private Button addWordBtn;
-    @FXML private TableView<WordEntry> table = new TableView<WordEntry>();
-    @FXML private TableColumn<WordEntry, String> wordColumn;
-    @FXML private TableColumn<WordEntry, String> translationColumn;
-    @FXML private Region addAndDeleteRegion;
-    @FXML private ToggleSwitch toggle;
-    @FXML private Label label;
 
+    @FXML private TabLearningController tabLearningController;
+    @FXML private TabWordsController tabWordsController;
+    @FXML private TextField filePath;
+    @FXML private Button fileChooser;
 
 
     @Override
     public void start(Stage stage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(Main.class.getClassLoader().getResource("sample/resources/NastiaMemo.fxml"));
+        FXMLLoader loader = new FXMLLoader(Main.class.getClassLoader().getResource("sample/resources/view/NastiaMemo.fxml"));
         loader.setController(this);
         root = loader.load();
         scene = new Scene(root);
@@ -59,81 +49,20 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
         scene.getStylesheets().add("sample/resources/mySwitch.css");
-        tabLearningController.init(this);
 
-        initTable();
         loadedFile = new File(System.getProperty("user.dir") + "\\LanguageMemo.txt");
         if (!loadedFile.exists()) {
             loadedFile.createNewFile();
         }
         wordEntryList = getWordEntryListFromFile(loadedFile);
         initialWordEntryList = getWordEntryListFromFile(loadedFile);
-        afterWordsListChanged();
-        setToggleSwitchOnMouseClicked();
-        wordField.setPopupEnabled(false);   //program start in AddWord mode, so we disable the popup
-        translationField.setPopupEnabled(false);    //program start in AddWord mode, so we disable the popup
-        //addAndDeleteRegion.setStyle("-fx-background-color: #ff4855");
-
-        table.setOnMouseClicked(mc -> {
-            if(!toggle.isSelected()){
-                WordEntry wordEntry = table.getSelectionModel().getSelectedItem();
-                wordField.setText(wordEntry.getWord());
-                wordField.hidePopup();
-                translationField.setText(wordEntry.getTranslation());
-                translationField.hidePopup();
-
-            }
-        });
 
         stage.setOnCloseRequest(we -> {
-            if(!wordEntryList.equals(initialWordEntryList))
-            {
-                String currentFilePath = filePath.getText();
-                Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Do you want to save changes to \"" + currentFilePath +"\"?", ButtonType.YES, ButtonType.NO);
-                alert.showAndWait();
-
-                if(alert.getResult() == ButtonType.YES) {
-                    WriteFileFromWordEntryList(loadedFile);
-                }
-            }
+            checkConditionAndWriteFileFromList();
         });
 
-        class WordAutoCompleteListener implements AutoCompleteSelectedEventListener {
-
-            //implement the required method(s) of the interface
-            @Override
-            public void handleAutoCompleteSelected(EventObject e, String selectedText){
-
-                String translation = wordEntryList.getEntryEquivalent(selectedText, WordEntryList.eEntries.WORD);
-                translationField.setText(translation);
-                translationField.hidePopup();
-            }
-        }
-
-        class TranslationAutoCompleteListener implements AutoCompleteSelectedEventListener {
-
-            //implement the required method(s) of the interface
-            @Override
-            public void handleAutoCompleteSelected(EventObject e, String selectedText){
-
-                String word = wordEntryList.getEntryEquivalent(selectedText, WordEntryList.eEntries.TRANSLATION);
-                wordField.setText(word);
-                wordField.hidePopup();
-            }
-        }
-
-        wordField.addEventListener(new WordAutoCompleteListener());
-        translationField.addEventListener(new TranslationAutoCompleteListener());
-
-    }
-
-    private void afterWordsListChanged() {
-        fillTable(wordEntryList);
-        recalculateWordsCounter();
-        wordField.clearEntries();
-        translationField.clearEntries();
-        wordField.getEntries().addAll(wordEntryList.getWordsAsList());
-        translationField.getEntries().addAll(wordEntryList.getTranslationsAsList());
+        tabLearningController.init(this);
+        tabWordsController.init(this);
 
     }
 
@@ -142,38 +71,26 @@ public class Main extends Application {
         launch(args);
     }
 
-    private void setToggleSwitchOnMouseClicked() {
-        toggle.setOnMouseClicked(t -> {
-            if (toggle.isSelected()){
-                toggle.setText("Add mode");
-                toggle.setStyle("-fx-base: limegreen");
-                addWordBtn.setDisable(false);
-                deleteWordBtn.setDisable(true);
-                wordField.setPopupEnabled(false);
-                translationField.setPopupEnabled(false);
 
-            }
-            else {
-                toggle.setText("Delete mode");
-                toggle.setStyle("-fx-base:  #ff4855");
-                addWordBtn.setDisable(true);
-                deleteWordBtn.setDisable(false);
-                wordField.setPopupEnabled(true);
-                translationField.setPopupEnabled(true);
+    private void checkConditionAndWriteFileFromList() {
+        if(!wordEntryList.equals(initialWordEntryList))
+        {
+            String currentFilePath = filePath.getText();
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Do you want to save changes to \"" + currentFilePath +"\"?", ButtonType.YES, ButtonType.NO);
+            alert.showAndWait();
 
+            if(alert.getResult() == ButtonType.YES) {
+                WriteFileFromWordEntryList(loadedFile);
             }
-            wordField.clear();
-            translationField.clear();
-            t.consume();
-        });
+        }
     }
 
     private void WriteFileFromWordEntryList(File loadedFile) {
 
         String streamedString = wordEntryList.getWordsList()
-                                            .stream()
-                                            .map(wordEntry -> wordEntry.getWord() + "," + wordEntry.getTranslation() + ";")
-                                            .collect(Collectors.joining());
+                .stream()
+                .map(wordEntry -> wordEntry.getWord() + "," + wordEntry.getTranslation() + ";")
+                .collect(Collectors.joining());
         try {
             FileWriter fileWriter = new FileWriter(loadedFile, false);
             fileWriter.write(streamedString);
@@ -185,15 +102,18 @@ public class Main extends Application {
         }
     }
 
-
     public void onOpenChooser(ActionEvent actionEvent) {
+
+        checkConditionAndWriteFileFromList();
+
         FileChooser chooser = new FileChooser();
         chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Memo text files", "*.txt"));
         chooser.setInitialDirectory(new File(System.getProperty("user.home") + "/Desktop"));
         try {
             loadedFile = chooser.showOpenDialog(scene.getWindow());
             wordEntryList = getWordEntryListFromFile(loadedFile);
-            afterWordsListChanged();
+            initialWordEntryList = getWordEntryListFromFile(loadedFile);
+            tabWordsController.afterWordsListChanged();
         }
         catch(NullPointerException e) {
             e.printStackTrace();
@@ -202,61 +122,6 @@ public class Main extends Application {
             e.printStackTrace();
             System.out.println("File not found.");
         }
-
-    }
-
-    public void onAddWordBtnClicked(ActionEvent actionEvent) {
-        String word = wordField.getText();
-        String translation = translationField.getText();
-
-        if(word.isEmpty()){
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Word field must not be empty.", ButtonType.OK);
-            alert.showAndWait();
-
-        }
-        else if(translation.isEmpty()){
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Translation field must not be empty.", ButtonType.OK);
-            alert.showAndWait();
-        }
-        else{
-            wordEntryList.addWord(new WordEntry(word, translation));
-            afterWordsListChanged();
-            wordField.clear();
-            translationField.clear();
-        }
-    }
-
-    public void onDeleteWordBtnClicked(ActionEvent actionEvent){
-        String word = wordField.getText();
-        String translation = translationField.getText();
-        WordEntry wordEntry = new WordEntry(word, translation);
-
-        if(word.isEmpty()){
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Word field must not be empty. Click on the table to load a word entry.", ButtonType.OK);
-            alert.showAndWait();
-
-        }
-        else if(translation.isEmpty()){
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Translation field must not be empty. Click on the table to load a word entry.", ButtonType.OK);
-            alert.showAndWait();
-        }
-        else if(wordEntryList.isWordEntryOnList(wordEntry)){
-            wordEntryList.deleteWord(wordEntry);
-            afterWordsListChanged();
-            wordField.clear();
-            translationField.clear();
-        }
-        else{
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "The word entry you want to delete doesn't exist in your dictionary.", ButtonType.OK);
-            alert.showAndWait();
-            wordField.clear();
-            translationField.clear();
-        }
-    }
-
-    private void recalculateWordsCounter(){
-        long lCounter = wordEntryList.getCount();
-        label.setText("  Your dictionary has:\n" + Long.toString(lCounter) + " entries");
 
     }
 
@@ -287,37 +152,13 @@ public class Main extends Application {
 
     }
 
-    private void fillTable(WordEntryList wordEntryList){
-
-        table.getColumns().clear();
-        data = FXCollections.observableArrayList(wordEntryList.getWordsList());
-        table.setItems(data);
-        table.getColumns().addAll(wordColumn, translationColumn);
-
-        /*data.removeAll();
-        data.addAll(wordEntryList.getWordsList());*/
-
-    }
-
-    private void initTable() {
-        wordColumn = new TableColumn("Word");
-        translationColumn = new TableColumn("Translation");
-        wordColumn.impl_setWidth(125);
-        translationColumn.impl_setWidth(122);
-
-        wordColumn.setCellValueFactory(
-                new PropertyValueFactory<WordEntry,String>("Word")
-        );
-        translationColumn.setCellValueFactory(
-                new PropertyValueFactory<WordEntry,String>("Translation")
-        );
-
-    }
-
     public WordEntryList getWordEntryList(){
         return this.wordEntryList;
     }
 
+    public File getLoadedFile(){
+        return this.loadedFile;
+    }
 
 }
 
---
+++ src/sample/TabWordsController.java
@@ -0,0 +1,212 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Region;
+import org.controlsfx.control.ToggleSwitch;
+
+import java.io.File;
+import java.util.EventObject;
+
+/**
+ * Created by Tomek on 28.03.2017.
+ */
+public class TabWordsController {
+
+    private Main main;
+    private File loadedFile;
+    private WordEntryList wordEntryList;
+
+    private ObservableList<WordEntry> data;
+
+    @FXML private AutoCompleteTextField wordField;
+    @FXML private AutoCompleteTextField translationField;
+    @FXML private Button deleteWordBtn;
+    @FXML private Button addWordBtn;
+    @FXML private TableView<WordEntry> table = new TableView<WordEntry>();
+    @FXML private TableColumn<WordEntry, String> wordColumn;
+    @FXML private TableColumn<WordEntry, String> translationColumn;
+    @FXML private Region addAndDeleteRegion;
+    @FXML private ToggleSwitch toggle;
+    @FXML private Label label;
+
+    public void init(Main mainController){
+        this.main = mainController;
+        this.loadedFile = main.getLoadedFile();
+        this.wordEntryList = main.getWordEntryList();
+
+        initTable();
+        afterWordsListChanged();
+        setToggleSwitchOnMouseClicked();
+        wordField.setPopupEnabled(false);   //program start in AddWord mode, so we disable the popup
+        translationField.setPopupEnabled(false);    //program start in AddWord mode, so we disable the popup
+        //addAndDeleteRegion.setStyle("-fx-background-color: #ff4855");
+
+        table.setOnMouseClicked(mc -> {
+            if(!toggle.isSelected()){
+                WordEntry wordEntry = table.getSelectionModel().getSelectedItem();
+                wordField.setText(wordEntry.getWord());
+                wordField.hidePopup();
+                translationField.setText(wordEntry.getTranslation());
+                translationField.hidePopup();
+
+            }
+        });
+
+        class WordAutoCompleteListener implements AutoCompleteSelectedEventListener {
+
+            //implement the required method(s) of the interface
+            @Override
+            public void handleAutoCompleteSelected(EventObject e, String selectedText){
+
+                String translation = wordEntryList.getEntryEquivalent(selectedText, WordEntryList.eEntries.WORD);
+                translationField.setText(translation);
+                translationField.hidePopup();
+            }
+        }
+
+        class TranslationAutoCompleteListener implements AutoCompleteSelectedEventListener {
+
+            //implement the required method(s) of the interface
+            @Override
+            public void handleAutoCompleteSelected(EventObject e, String selectedText){
+
+                String word = wordEntryList.getEntryEquivalent(selectedText, WordEntryList.eEntries.TRANSLATION);
+                wordField.setText(word);
+                wordField.hidePopup();
+            }
+        }
+
+        wordField.addEventListener(new WordAutoCompleteListener());
+        translationField.addEventListener(new TranslationAutoCompleteListener());
+
+    }
+
+    public void afterWordsListChanged() {
+        this.loadedFile = main.getLoadedFile();
+        this.wordEntryList = main.getWordEntryList();
+        fillTable(wordEntryList);
+        recalculateWordsCounter();
+        wordField.clearEntries();
+        translationField.clearEntries();
+        wordField.getEntries().addAll(wordEntryList.getWordsAsList());
+        translationField.getEntries().addAll(wordEntryList.getTranslationsAsList());
+
+    }
+
+
+
+    private void setToggleSwitchOnMouseClicked() {
+        toggle.setOnMouseClicked(t -> {
+            if (toggle.isSelected()){
+                toggle.setText("Add mode");
+                toggle.setStyle("-fx-base: limegreen");
+                addWordBtn.setDisable(false);
+                deleteWordBtn.setDisable(true);
+                wordField.setPopupEnabled(false);
+                translationField.setPopupEnabled(false);
+
+            }
+            else {
+                toggle.setText("Delete mode");
+                toggle.setStyle("-fx-base:  #ff4855");
+                addWordBtn.setDisable(true);
+                deleteWordBtn.setDisable(false);
+                wordField.setPopupEnabled(true);
+                translationField.setPopupEnabled(true);
+
+            }
+            wordField.clear();
+            translationField.clear();
+            t.consume();
+        });
+    }
+
+    public void onAddWordBtnClicked(ActionEvent actionEvent) {
+        String word = wordField.getText();
+        String translation = translationField.getText();
+
+        if(word.isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Word field must not be empty.", ButtonType.OK);
+            alert.showAndWait();
+
+        }
+        else if(translation.isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Translation field must not be empty.", ButtonType.OK);
+            alert.showAndWait();
+        }
+        else{
+            wordEntryList.addWord(new WordEntry(word, translation));
+            afterWordsListChanged();
+            wordField.clear();
+            translationField.clear();
+        }
+    }
+
+    public void onDeleteWordBtnClicked(ActionEvent actionEvent){
+        String word = wordField.getText();
+        String translation = translationField.getText();
+        WordEntry wordEntry = new WordEntry(word, translation);
+
+        if(word.isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Word field must not be empty. Click on the table to load a word entry.", ButtonType.OK);
+            alert.showAndWait();
+
+        }
+        else if(translation.isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Translation field must not be empty. Click on the table to load a word entry.", ButtonType.OK);
+            alert.showAndWait();
+        }
+        else if(wordEntryList.isWordEntryOnList(wordEntry)){
+            wordEntryList.deleteWord(wordEntry);
+            afterWordsListChanged();
+            wordField.clear();
+            translationField.clear();
+        }
+        else{
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "The word entry you want to delete doesn't exist in your dictionary.", ButtonType.OK);
+            alert.showAndWait();
+            wordField.clear();
+            translationField.clear();
+        }
+    }
+
+    private void recalculateWordsCounter(){
+        long lCounter = wordEntryList.getCount();
+        label.setText("  Your dictionary has:\n" + Long.toString(lCounter) + " entries");
+
+    }
+
+
+    private void fillTable(WordEntryList wordEntryList){
+
+        table.getColumns().clear();
+        data = FXCollections.observableArrayList(wordEntryList.getWordsList());
+        table.setItems(data);
+        table.getColumns().addAll(wordColumn, translationColumn);
+
+        /*data.removeAll();
+        data.addAll(wordEntryList.getWordsList());*/
+
+    }
+
+    private void initTable() {
+        wordColumn = new TableColumn("Word");
+        translationColumn = new TableColumn("Translation");
+        wordColumn.impl_setWidth(125);
+        translationColumn.impl_setWidth(122);
+
+        wordColumn.setCellValueFactory(
+                new PropertyValueFactory<WordEntry,String>("Word")
+        );
+        translationColumn.setCellValueFactory(
+                new PropertyValueFactory<WordEntry,String>("Translation")
+        );
+
+    }
+
+}
