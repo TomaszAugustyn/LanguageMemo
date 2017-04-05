35a3fe78167669771e3ece7c721777372dc03c5e
Tomasz Augustyn <lourian@poczta.fm>
2017-04-06 00:11:27 +0200

    Setting/reseting gradient style, new SessionContainer Class, + refactoring

--- src/sample/TabLearningController.java
+++ src/sample/TabLearningController.java
@@ -5,15 +5,38 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.lang.Math.min;
+import static javafx.scene.paint.CycleMethod.NO_CYCLE;
 
 /**
  * Created by Tomek on 26.03.2017.
  */
 public class TabLearningController {
+
+    private static class EnglishLblGradientStyle{
+        private static Stop[] stops = new Stop[] { new Stop(0, Color.BLACK), new Stop(1, Color.TRANSPARENT)};
+        private static LinearGradient gradient = new LinearGradient(1.0, 0, 1.0, 0.8428571428571429, true, CycleMethod.NO_CYCLE, stops);
+    }
+
+    private static class PolishLblGradientStyle{
+        private static Stop[] stops = new Stop[] { new Stop(0, Color.BLACK), new Stop(1, Color.WHITE)};
+        private static LinearGradient gradient = new LinearGradient(1.0, 0.9476190476190476, 1.0, 1.0, true, CycleMethod.REFLECT, stops);
+    }
+
+    private static class SessionContainer{
+        private static boolean sessionStarted = false;
+        private static int nrOfWordsPerSession = 0;
+        private static List<WordEntry> randomizedWordList = new ArrayList<>();
+        private static int translationMode = 0;
+    }
 
     @FXML public Button startBtn;
     @FXML private RadioButton p2eRadio;
@@ -26,7 +49,6 @@ public class TabLearningController {
 
     private ToggleGroup toggleGroup = new ToggleGroup();
     private WordEntryList wordEntryList;
-    private boolean sessionStarted = false;
 
     private Main main;
 
@@ -50,26 +72,41 @@ public class TabLearningController {
     }
 
     @FXML private void onStartBtnClicked(ActionEvent actionEvent){
-        if(sessionStarted){
+        adjustControlsToSessionState();
+        toggleSessionState();
+
+        if(SessionContainer.sessionStarted){
+            wordEntryList = main.getWordEntryList();
+            SessionContainer.nrOfWordsPerSession = getNrOfWordsPerSession();
+            SessionContainer.randomizedWordList =  wordEntryList.getNRandomUniqueWordEntries(SessionContainer.nrOfWordsPerSession);
+            SessionContainer.translationMode = e2pRadio.isSelected()? WordEntry.ENG_2_POL : WordEntry.POL_2_ENG;
+
+            System.out.println(SessionContainer.randomizedWordList);
+        }
+
+    }
+
+    private void adjustControlsToSessionState() {
+        if(SessionContainer.sessionStarted){
             startBtn.setText("Start learning");
             startBtn.setStyle("-fx-background-color: mediumpurple; -fx-border-color: black;");
             enterBtn.setDisable(true);
-            sessionStarted = false;
+            englishWordLabel.setText("Press \"Start learning\"");
+            polishWordLabel.setText("Press \"Start learning\"");
+            englishWordLabel.setTextFill(EnglishLblGradientStyle.gradient);
+            polishWordLabel.setTextFill(PolishLblGradientStyle.gradient);
+
         }else{
             startBtn.setText("Stop learning");
             startBtn.setStyle("-fx-background-color: #ff4855; -fx-border-color: black;");
             enterBtn.setDisable(false);
-            sessionStarted = true;
+            englishWordLabel.setTextFill(Color.BLACK);
+            polishWordLabel.setTextFill(Color.BLACK);
         }
+    }
 
-        if(sessionStarted){
-            wordEntryList = main.getWordEntryList();
-            int nrOfWordsPerSession = getNrOfWordsPerSession();
-            List<WordEntry> wordsSubList =  wordEntryList.getNRandomUniqueWordEntries(nrOfWordsPerSession);
-
-            //System.out.println(wordsSubList);
-        }
-
+    private void toggleSessionState() {
+        SessionContainer.sessionStarted = !SessionContainer.sessionStarted;
     }
 
     private int getNrOfWordsPerSession() {
